c8c7b781c25817fa4aa80140fbb1312c61a12581
All Eyes <matthewacha@gmail.com>
2017-10-02 23:07:14 +0300

    Add login_manager

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from sqlalchemy.orm import relationship
-from app import db
+from app import db, login_manager
 
 
 class User(db.Model, UserMixin):
