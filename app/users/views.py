0877b2fb2754cac8a9cc11762d93331e96f04135
All Eyes <matthewacha@gmail.com>
2017-10-02 23:08:20 +0300

    Refactor signup controller

--- app/users/views.py
+++ app/users/views.py
@@ -1,22 +1,25 @@
 from flask import Flask, jsonify, request, session
 from app.models import User
 from . import users
-from ..app import db
+from .. import db
 
-@users.route('/api/signup', methods=['POST'])
+@users.route('/api/user/signup', methods=['POST'])
 def signup():
     json_data = request.json
     user = User(first_name = json_data['first_name'],\
                  last_name = json_data['last_name'],\
                  email = json_data['email'],\
                  password = json_data['password'])
-    db.session.add(user)
-    db.session.commit()
-    message = 'Successfully signed up'
+    try:
+        db.session.add(user)
+        db.session.commit()
+        message = 'Successfully signed up'
+    except:
+        pass
     db.session.close()
     return jsonify({'message':message})
 
-@users.route('/api/login', methods = ['POST'])
+@users.route('/api/user/login', methods = ['POST'])
 def login():
     json_data = request.json
     user = User.query.fetch_by(email = json_data['email']).first()
