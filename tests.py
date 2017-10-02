cfdb70a4c7c1528ad8efaa866ee325d8bc7bf029
All Eyes <matthewacha@gmail.com>
2017-10-02 23:09:52 +0300

    Write test for signup endpoint

--- tests.py
+++ tests.py
@@ -1,7 +1,15 @@
 import unittest
-from app import views
+from app.users import views
+from app import app
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
 
-def BaseTest(unittest.Testcase):
+    def test_user_login(self):
+        info = dict(first_name = 'James', last_name = 'Clark',\
+                    email = 'jc@gmail.com', password = 'mandelson')
+        response = self.app.post('/api/user/signup', data=info)
+        self.assertEqual(response.status_code, 201)
 
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
