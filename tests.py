2dd7c13fe6c7d70e66ca5a4b5a7bdda207ce00cb
All Eyes <matthewacha@gmail.com>
2017-10-03 11:04:20 +0300

    Create test cases for views

--- tests.py
+++ tests.py
@@ -5,11 +5,16 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_user_login(self):
+    def test_user_signup(self):
         info = dict(first_name = 'James', last_name = 'Clark',\
                     email = 'jc@gmail.com', password = 'mandelson')
         response = self.app.post('/api/user/signup', data=info)
+        self.assertIn(b'',response.data)
         self.assertEqual(response.status_code, 201)
+        
+
+    def test_user_login(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
